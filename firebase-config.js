24ca58a7b91a99aa149bc2a103f5db66914a3146
astraconnect-cyber <ezeugwuemmanuel43@gmail.com>
2026-09-19 21:43:05 +0100

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAF3ema-3PGr7qmsh2YwJEfjPQbcaaHq7M",
+  authDomain: "miragracematt.firebaseapp.com",
+  projectId: "miragracematt",
+  storageBucket: "miragracematt.firebasestorage.app",
+  messagingSenderId: "857477562524",
+  appId: "1:857477562524:web:cedac6e33c6c500ac6ed59"
+};
